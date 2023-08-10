3bc67276038176c16e5f6c1f034633f8bc9b3ddd
Souhardya Das <dassouhardya79@gmail.com>
2023-08-10 18:00:29 +0530

    files modified

--- handson1/handson1.go
+++ handson1/handson1.go
@@ -1,15 +1,41 @@
 package main
-import(
+
+import (
 	"fmt"
+	"math"
 )
-func main(){
-	xi:=[]int{1,2,3,4,5,6,7,8}
-	fmt.Println(foo(xi...))
+
+type square struct {
+	length float64
+	width  float64
 }
-func foo(ii ...int)int{
-	t:=0
-	for_,v:=range ii {
-		t += v
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.width
+}
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+type shape interface {
+	area() float64
+}
+
+func info(s shape) float64 {
+	return s.area()
+}
+func main() {
+	c1 := circle{
+		radius: 4,
 	}
-	return t
+	s1 := square{
+		length: 5,
+		width:  8,
+	}
+	fmt.Println(info(c1))
+	fmt.Println(info(s1))
+
 }
